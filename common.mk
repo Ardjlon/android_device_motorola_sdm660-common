df5b556042cbfde79981b41206e4a6bb09c5788c
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sdm660-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- common.mk
+++ common.mk
@@ -344,8 +344,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power-service
+    android.hardware.power-service-qti
 
 # Protobuf
 PRODUCT_PACKAGES += \
