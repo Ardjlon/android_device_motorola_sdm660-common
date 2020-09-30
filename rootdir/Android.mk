c7579f57649c2066de96953ec20663f3d5a1cbee
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-30 00:39:22 +0000

    sdm660-common: Safe - Bypass safetynet even if we have permissive selinux - Crop SafetyNet Status in Magisk - This change are inspired in Magisk Source Code
    Signed-off-by: kytoaq <kytoaq@gmail.com>
    Signed-off-by: topjohnwu <topjohnwu@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    All files made created by Pixel Experience Core Team

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -126,6 +126,16 @@ LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := root/ueventd.qcom.rc
+
 # this needs to be in /vendor/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+# Safail NET
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
